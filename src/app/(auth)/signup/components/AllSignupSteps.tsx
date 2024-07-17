710df5e97936c19b47460d79cb9db7bc964ba7c2
aouiniamine <amine.m.aouini@hotmail.com>
2024-07-17 19:33:29 +0100

    setup to generate token

--- src/app/(auth)/signup/components/AllSignupSteps.tsx
+++ src/app/(auth)/signup/components/AllSignupSteps.tsx
@@ -3,15 +3,17 @@ import { useAtomValue, useSetAtom } from 'jotai'
 import SignupForm from './SignupForm'
 import SignupMembershipContent from './membership/SignupMembershipContent'
 import EmailVerificationContent from './email-verification/components/EmailVerificationContent.tsx'
+import SignupPayment from './SignupPayment'
 
 export default function AllSignupSteps() {
 
   const signupStep = useAtomValue(singupStepAtom)
   return (
     <>
-        { signupStep === 1 && (<SignupForm/>) }
+        {/* { signupStep === 1 && (<SignupForm/>) }
         { signupStep === 2 && (<EmailVerificationContent />)}
-        { signupStep === 3 && (<SignupMembershipContent />)}
+        { signupStep === 3 && (<SignupMembershipContent />)} */}
+        <SignupPayment/>
     </>
   )
 }
--- src/components/atoms/PaymentMethod.tsx
+++ src/components/atoms/PaymentMethod.tsx
@@ -1,10 +1,15 @@
 import { useEffect } from "react";
 import { loadScript } from "@paypal/paypal-js";
+import axios from "axios"
 export default function PaymentMethod() {
     useEffect(()=>{
-        
+        axios.post("localhost:5000//api/generate/token")
+        .then((token)=>{
+            
+        })
         loadScript({clientId: "AQTLR4qk4C3cDL7oMT6GN8oxoQ-pySBWicypWAAAELk1f5YD8Yx1dt5DXSVQJX9raTMWx3va9ebXhREW", components: "buttons,hosted-fields", currency: ""})
         .then(paypal => {
+            
             paypal?.Buttons({style: {
                 shape: "pill"
             }}).render("#payment-options")
