1c3c9be8a7def7a27355c9cb50e3299b67320246
jazwu <jaz.wu@outlook.com>
2024-11-08 12:51:12 -0500

    refactor: move contact-us fetch into  src\lib\api\contactService.ts

--- src/lib/api/userService.ts
+++ src/lib/api/userService.ts
@@ -25,3 +25,19 @@ export const updateUser = async (fullName: string, displayName: string, email: s
     const data = await response.json()
     return {data, ok: response.ok}
 }
+
+export const contactUs = async (name: string, email: string, message: string) => {
+    const response = await fetch('/api/contact-us/', {
+        method: "POST", 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            email,
+            message
+        })
+    })
+    const data = await response.json()
+    return {data, ok: response.ok}
+}
