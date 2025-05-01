948658f85d53ce9a08bf2eb4b471669d9eed627c
atharvasalehittal <atharva.salehittal@gmail.com>
2025-05-01 15:32:03 -0400

    Sign-Up 001 Notiff Bar error resolved

--- src/app/(authorized)/account/settings/profile/components/SuccessPopup.tsx
+++ src/app/(authorized)/account/settings/profile/components/SuccessPopup.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 export default function SuccessPopup({closeSuccessPopup}: {closeSuccessPopup: any}) {
     const isNavOpen = useAtomValue(IsNavOpenAtom)
   return (
-    <div className="absolute flex items-center justify-between h-[6vh] w-[78vw] bottom-[1vh] bg-primary-yellow-accent rounded-2xl" style={isNavOpen ? {width: '70vw'} : {}}>
+    < /* div className="absolute flex items-center justify-between h-[6vh] w-[78vw] bottom-[1vh] bg-primary-yellow-accent rounded-2xl" style={isNavOpen ? {width: '70vw'} : {}} */ div className="fixed z-50 flex items-center justify-between h-[48px] w-[78vw] bottom-[1vh] bg-primary-yellow-accent rounded-2xl" style={isNavOpen ? { width: '70vw' } : {} } >
         <div className="flex items-center">
             <div className="relative h-[3vh] w-[3vh] ml-[1vw]">
                 <Image src="/about/membership/Icon-include.svg" fill alt="" />
