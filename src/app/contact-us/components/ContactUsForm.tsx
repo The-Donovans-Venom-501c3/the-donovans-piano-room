ffde283944268635b1426e09e1e5e1199e0154b6
Jiaqi-Ye <jye224@wisc.edu>
2025-06-10 00:25:53 +0800

    Fixed merge wrong branch

--- src/app/contact-us/components/ContactUsForm.tsx
+++ src/app/contact-us/components/ContactUsForm.tsx
@@ -63,17 +63,16 @@ export default function ContactUsForm() {
         )}
         {error && <p>{error}</p>}
       </div>
-      <div className="flex flex-col items-end justify-center md:flex-row">
+      <div className="mb-8 flex flex-col items-start">
+        <h2 className="mb-8 text-6xl font-semibold">Get in touch</h2>
+        <p className="md:w-1/2 text-xl">
+          Have any questions, concerns, or need assistance? Fill out the form
+          below and one of our representatives will reach out to you in a timely
+          manner.
+        </p>
+      </div>
+      <div className="flex flex-col items-start justify-between md:flex-row">
         <div className="mb-10 w-full md:mb-0 md:mr-10 md:w-1/2">
-
-          <div className="mb-8 flex flex-col items-start">
-            <h2 className="mb-8 text-6xl font-semibold">Get in touch</h2>
-            <p className="md:w-2/3 text-xl">
-              Have any questions, concerns, or need assistance? Fill out the form
-              below and one of our representatives will reach out to you in a timely
-              manner.
-            </p>
-          </div>
           <form className="space-y-4" onSubmit={handleSubmit}>
             <InputForm
               field={{
@@ -116,7 +115,7 @@ export default function ContactUsForm() {
             </div>
           </form>
         </div>
-        <div className="relative -top-20 w-full rounded-2xl bg-yellow-500 text-primary-brown md:w-1/3">
+        <div className="w-full rounded-2xl bg-yellow-500 text-primary-brown md:w-1/3">
           <div className="p-5 font-medium">
             <div className="mb-5 flex">
               <Image src="/Info.svg" width={25} height={100} alt="Info" />
@@ -135,7 +134,7 @@ export default function ContactUsForm() {
               <Link
                 href="https://twitter.com/IamTheDonovan"
                 target="_blank"
-                className="w-15 h-15 flex items-center justify-between rounded-full text-white transition duration-300 hover:bg-purple-800"
+                className="w-15 h-15 flex items-center justify-center rounded-full text-white transition duration-300 hover:bg-purple-800"
               >
                 <Image
                   src="/footer/Twitter.svg"
@@ -147,7 +146,7 @@ export default function ContactUsForm() {
               <Link
                 href="https://www.facebook.com/TheDonovansVenomINC/"
                 target="_blank"
-                className="w-15 h-15 flex items-center justify-between rounded-full text-white transition duration-300 hover:bg-purple-800"
+                className="w-15 h-15 flex items-center justify-center rounded-full text-white transition duration-300 hover:bg-purple-800"
               >
                 <Image
                   src="/footer/Facebook.svg"
@@ -159,7 +158,7 @@ export default function ContactUsForm() {
               <a
                 href="https://www.youtube.com/@thedonovansvenom2848"
                 target="_blank"
-                className="w-15 h-15 flex items-center justify-between rounded-full text-white transition duration-300 hover:bg-purple-800"
+                className="w-15 h-15 flex items-center justify-center rounded-full text-white transition duration-300 hover:bg-purple-800"
               >
                 <Image
                   src="/footer/Youtube.svg"
