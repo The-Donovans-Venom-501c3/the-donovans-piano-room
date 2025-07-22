849ff705195542abf9dcf21c8bce6c1e72e40e67
Jiaqi-Ye <jye224@wisc.edu>
2025-07-23 06:39:42 +0800

    remove useless part

--- src/app/cart/components/ItemsAddedCart.tsx
+++ src/app/cart/components/ItemsAddedCart.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PaymentCard from "./PaymentCard";
 import ListedItemCard from "./ListedItemCard";
-import { useAtom } from 'jotai';
+import { useAtom } from "jotai";
 import { addedCartItemsAtom } from "@/store/cartStore";
 
 export default function ItemsAddedCart() {
@@ -10,7 +10,6 @@ export default function ItemsAddedCart() {
   return (
     <div className="flex flex-col lg:flex-row w-full h-full justify-between tablet:gap-8 laptop:gap-10 desktop:gap-12 tablet:pb-[6%]">
       <div className="flex flex-col tablet:w-[60%] tablet:h-[72%] desktop:w-[80%] desktop:h-full gap-8">
-
         <div className="bg-white rounded-2xl shadow-md p-6 mt-6">
           <div className="flex justify-between items-center border-b-2 border-purple-200 pb-5 mb-6 px-6 mx-2">
             <div className="font-montserrat font-bold text-3xl text-primary-brown mt-6">
@@ -29,9 +28,7 @@ export default function ItemsAddedCart() {
           </div>
         </div>
       </div>
-      <PaymentCard onCheckoutPayPalClick={function (): void {
-        throw new Error("Function not implemented.");
-      }} />
+      <PaymentCard />
     </div>
   );
 }
