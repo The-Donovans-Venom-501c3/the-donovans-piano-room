dadf3b413382b38954914ffb076428a50eb29935
techvolunteer4-tech <tech.volunteer4@thedonovan.org>
2026-06-16 04:53:30 -0500

    Add files via upload

---
+++ lib/api/userService.ts
@@ -0,0 +1,28 @@
+import { UserUpdateData } from "@/interfaces/profileInterface"
+import exp from "constants"
+
+export const getUser = async () =>{
+    try{
+        const response = await fetch('/api/user/', {
+            method: "GET", 
+            credentials: 'include'
+        })
+        const data = await response.json()
+        return {data, ok: response.ok}
+    }catch(err){
+        return {ok:false}
+    }
+    
+}
+
+export const updateUser = async (userData: UserUpdateData) => {
+    const response = await fetch('/api/user/', {
+        method: "PUT", 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    })
+    const data = await response.json()
+    return {data, ok: response.ok}
+}
